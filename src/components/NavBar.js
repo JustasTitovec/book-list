0ce876e82e5999fdd6deb39a28aee059ae1ce35a
Justas Titovec <justastitovec@Justass-MacBook-Pro.local>
2020-09-03 22:44:25 +0300

    booklist

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
+
+function NavBar() {
+  const { books } = useContext(BookContext);
+  return (
+    <div className="navbar">
+      <h1>Justas reading list</h1>
+      <p>Currently you have {books.length} books</p>
+    </div>
+  );
+}
+
+export default NavBar;
